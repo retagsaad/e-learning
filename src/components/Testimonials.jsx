74a0a606f85f21fc5fb07b73fd7c4d7f5af6e0a0
retag <retagsaad77@gmail.com>
2026-05-08 21:21:28 +0300

    testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,40 @@
+import { testimonials } from "@/data";
 import React from "react";
-
+import Image from "next/image";
+import { FaStar } from "react-icons/fa";
 export default function Testimonials() {
-  return <div>Testimonials</div>;
+  return (
+    <div className="my-10">
+      <div className="md:flex md:gap-2 xl:gap-10 justify-center">
+        {testimonials.map((testimonial) => (
+          <div
+            key={testimonial.id}
+            className="my-5 bg-indigo-100 p-4 rounded-xl shadow-xl sm:w-85"
+          >
+            <div className="flex gap-2">
+              <Image
+                src={testimonial.image}
+                width={35}
+                height={35}
+                alt="user"
+                className="rounded-4xl"
+              />
+              <div className="">
+                <h3 className="font-bold">{testimonial.name}</h3>
+                <p className="text-gray-500">{testimonial.role}</p>
+              </div>
+            </div>
+            <div className="flex my-4 text-lg">
+              <FaStar className="text-yellow-400" />
+              <FaStar className="text-yellow-400" />
+              <FaStar className="text-yellow-400" />
+              <FaStar className="text-yellow-400" />
+              <FaStar className="text-yellow-400" />
+            </div>
+            <p className="w-75">{testimonial.review}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
